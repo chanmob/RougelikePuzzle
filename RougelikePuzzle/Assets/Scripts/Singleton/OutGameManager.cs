97f759633f84da80ce307d50809d91203985d6ed
ParkJunYeong <junb0sday@gmail.com>
2021-04-02 19:48:50 +0900

    OutGame 골드 표시 및 골드 추가 버튼 생성

---
+++ RougelikePuzzle/Assets/Scripts/Singleton/OutGameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutGameManager : Singleton<OutGameManager>
+{
+    private int _gold = 0;
+	private Text _goldText;
+
+	private void Start() {
+		_goldText = GameObject.Find("GoldText").GetComponent<Text>();
+	}
+
+	public void AddGold(int getGold) {
+		_gold += getGold;
+		UIGoldTextRefresh();
+	}
+
+	public int GetGold() {
+		return _gold;
+	}
+
+	public void UIGoldTextRefresh() {
+		_goldText.text = string.Format("Gold : {0}", _gold);
+	}
+}
