039da2a771fe2cd7ee1c1835d208b592634f2e00
chanmob <sellingout@naver.com>
2021-04-03 09:22:59 +0900

    Addresable Asset 삭제

--- RougelikePuzzle/Assets/Scripts/Singleton/DataManager.cs
+++ RougelikePuzzle/Assets/Scripts/Singleton/DataManager.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class DataManager : Singleton<DataManager>
 {
@@ -29,97 +27,17 @@ public class DataManager : Singleton<DataManager>
         UnityEngine.SceneManagement.SceneManager.LoadScene("InGame");
     }
 
-
     protected override void OnAwake()
     {
-        Addressables.LoadAssetAsync<GameObject>("Bomb").Completed += AddressableToBomb;
-        Addressables.LoadAssetAsync<GameObject>("CardReset").Completed += AddressableToCardReset;
-        Addressables.LoadAssetAsync<GameObject>("ChangeCardPosition").Completed += AddressableToChangeCardPosition;
-        Addressables.LoadAssetAsync<GameObject>("Coin").Completed += AddressableToCoin;
-        Addressables.LoadAssetAsync<GameObject>("Flame").Completed += AddressableToFlameThrower;
-        Addressables.LoadAssetAsync<GameObject>("Monster").Completed += AddressableToMonster;
-        Addressables.LoadAssetAsync<GameObject>("Potion").Completed += AddressableToPotion;
-        Addressables.LoadAssetAsync<GameObject>("Thorn").Completed += AddressableToThorn;
-        Addressables.LoadAssetAsync<GameObject>("Weapon").Completed += AddressableToWeapon;
-    }
-
-    public void AddressableToBomb(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            bomb = obj.Result.GetComponent<Bomb>();
-        }
-    }
-
-    public void AddressableToCardReset(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            cardReset = obj.Result.GetComponent<CardReset>();
-        }
-    }
-
-    public void AddressableToChangeCardPosition(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            changeCardPosition = obj.Result.GetComponent<ChangeCardPosition>();
-        }
-    }
-
-    public void AddressableToWeapon(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            weapon = obj.Result.GetComponent<Weapon>();
-        }
-    }
-
-    public void AddressableToCoin(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            coin = obj.Result.GetComponent<Coin>();
-        }
-    }
-
-    public void AddressableToFlameThrower(AsyncOperationHandle<GameObject> obj)
-    {
-        if(obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            flameThrower = obj.Result.GetComponent<FlameThrower>();
-        }
-    }
-
-    public void AddressableToFlameThrower2Way(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            flameThrower2Way = obj.Result.GetComponent<FlameThrower2Way>();
-        }
-    }
-
-    public void AddressableToThorn(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            thorn = obj.Result.GetComponent<Thorn>();
-        }
-    }
-
-    public void AddressableToMonster(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            monster = obj.Result.GetComponent<Monster>();
-        }
-    }
-
-    public void AddressableToPotion(AsyncOperationHandle<GameObject> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Succeeded)
-        {
-            potion = obj.Result.GetComponent<RedPotion>();
-        }
+        bomb = Resources.Load<Bomb>("Objects/Bomb");
+        cardReset = Resources.Load<CardReset>("Objects/CardReset");
+        changeCardPosition = Resources.Load<ChangeCardPosition>("Objects/ChangeCardPosition");
+        weapon = Resources.Load<Weapon>("Objects/Weapon");
+        coin = Resources.Load<Coin>("Objects/Coin");
+        //flameThrower2Way = Resources.Load<FlameThrower2Way>("Objects/FlameThrower2Way");
+        flameThrower = Resources.Load<FlameThrower>("Objects/Flame");
+        thorn = Resources.Load<Thorn>("Objects/Thorn");
+        monster = Resources.Load<Monster>("Objects/Monster");
+        potion = Resources.Load<RedPotion>("Objects/Potion");
     }
 }
