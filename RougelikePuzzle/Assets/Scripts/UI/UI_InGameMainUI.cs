c60bae68fd596027df55f922aab624aa11b54902
chanmob <sellingout@naver.com>
2021-04-14 22:29:58 +0900

    무기판매 추가

--- RougelikePuzzle/Assets/Scripts/UI/UI_InGameMainUI.cs
+++ RougelikePuzzle/Assets/Scripts/UI/UI_InGameMainUI.cs
@@ -20,7 +20,13 @@ public class UI_InGameMainUI : MonoBehaviour
 
     public void SellWeapon()
     {
-
+        if(InGameManager.instance.player.weaponType != Define.WeaponType.None)
+        {
+            InGameManager.instance.player.weapon = null;
+            InGameManager.instance.player.weaponType = Define.WeaponType.None;
+            InGameManager.instance.player.weaponDurability = 0;
+            InGameManager.instance.player.PlayerHPTextRefresh();
+        }
     }
 
     public void OnSettingUI()
