1ad9dd1a86eb07d93e1dd54873dbcbdbd23a6f14
ParkJunYeong <junb0sday@gmail.com>
2021-07-04 23:43:30 +0900

    골드 뽑기 구현 완료

---
+++ RougelikePuzzle/Assets/Scripts/OutGame/Gacha/GachaInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GachaInfo : MonoBehaviour
+{
+	[SerializeField] private Text costText;
+	[SerializeField] private GachaResult gachaResult;
+	private int cost = 1000;
+
+	public void SetInfo() {
+		costText.text = cost.ToString();
+	}
+
+	public void OnPressGachaButton() {
+		if(OutGameManager.instance._GoldData._Gold < cost) return;
+		OutGameManager.instance._GoldData.AddGold(-cost);
+		gachaResult.gameObject.SetActive(true);
+		gachaResult.GachaAnimation();
+		gameObject.SetActive(false);
+	}
+}
---
+++ RougelikePuzzle/Assets/Scripts/OutGame/Gacha/GachaResult.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GachaResult : MonoBehaviour
+{
+	[SerializeField] private Image Chest;
+	[SerializeField] private Image MaskingTarget;
+	[SerializeField] private GameObject GachaEffect;
+	[SerializeField] private GameObject GachaResultFrame;
+	[SerializeField] private Text RandomAmountText;
+
+	public void GachaAnimation() {
+		Chest.transform.DOKill();
+		GachaEffect.SetActive(true);
+		GachaResultFrame.SetActive(false);
+		MaskingTarget.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+		MaskingTarget.DOColor(Color.white, 2.0f).OnComplete(OnGachaAnimationComplete);
+		Chest.transform.rotation = Quaternion.AngleAxis(-10.0f, Vector3.forward);
+		Chest.transform.DORotate(Vector3.forward * 10.0f, 0.2f).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.Linear);
+	}
+
+	public void OnGachaAnimationComplete() {
+		GachaEffect.SetActive(false);
+		GachaResultFrame.SetActive(true);
+		GetCoin();
+	}
+	public void GetCoin() {
+		int coinRandomValue = GetRandomAmount(0, 2000);
+		RandomAmountText.text = string.Format("X {0}", coinRandomValue);
+		OutGameManager.instance._GoldData.AddGold(coinRandomValue);
+	}
+	public int GetRandomAmount(int minValue, int maxValue) {
+		return Random.Range(minValue, maxValue + 1);
+	}
+	
+}
