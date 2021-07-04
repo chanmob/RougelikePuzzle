9dba08e486c255a546687340d6742307295bb233
ParkJunYeong <junb0sday@gmail.com>
2021-07-04 21:18:28 +0900

    아웃게임UI 좌우스크롤 구현 및 페이지 변경 버튼 추가

---
+++ RougelikePuzzle/Assets/Scripts/UI/UI_OutGamePageControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class UI_OutGamePageControl : MonoBehaviour
+{
+    [SerializeField]
+    private UI_Page[] uiPages;
+    private int recentPage;
+    private Vector3[] uiPagePositions;
+    [SerializeField]
+    private Button[] pageChangeButtons;
+
+    public void Awake() {
+        uiPagePositions = new Vector3[uiPages.Length];
+        for(int i = 0; i < uiPages.Length; i++) {
+            uiPages[i].Init(i);
+            uiPagePositions[i] = uiPages[i].transform.position;
+		}
+
+        //ChangePageNoTween(2);
+    }
+
+    public void ChangePage(int TargetPage) {
+        recentPage = TargetPage;
+        for(int i = 0; i < uiPages.Length; i++) {
+            uiPages[i].transform.DOMoveX(uiPagePositions[i].x - recentPage * Screen.width, 0.4f);
+            pageChangeButtons[i].image.color = i == recentPage ? Color.gray : Color.white;
+		}
+	}
+	
+    public void ChangePageNoTween(int TargetPage) {
+        recentPage = TargetPage;
+        for (int i = 0; i < uiPages.Length; i++) {
+            uiPages[i].transform.position = new Vector3(uiPagePositions[i].x - recentPage * Screen.width, uiPagePositions[i].y, uiPagePositions[i].z);
+            pageChangeButtons[i].image.color = i == recentPage ? Color.gray : Color.white;
+        }
+    }
+
+}
---
+++ RougelikePuzzle/Assets/Scripts/UI/UI_Page.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Page : MonoBehaviour
+{
+	[SerializeField]
+	private RectTransform pageRectTransform;
+
+	public void Init(int i) {
+		pageRectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+		pageRectTransform.position = new Vector3(i * Screen.width + Screen.width / 2, pageRectTransform.position.y, pageRectTransform.position.z);
+	}
+}
